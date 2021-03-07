64300019f6505cee10ac2414524cd070211517b8
coppa <sortal@protonmail.com>
2021-03-07 17:25:26 +0900

    Initial Commit

---
+++ hso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hso
+//
+//  Created by 橘葉月 on 2021/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hso/hsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  hsoApp.swift
+//  hso
+//
+//  Created by 橘葉月 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct hsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
